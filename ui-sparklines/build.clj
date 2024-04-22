c3834ab00e86dc02275b0ef44d2594eff68e81b0
awb99 <awb99@gmail.com>
2024-04-22 12:00:40 -0500

    clojars licence added

--- ui-sparklines/build.clj
+++ ui-sparklines/build.clj
@@ -8,11 +8,26 @@
 (def lib 'org.pinkgorilla/ui-sparklines)
 (def version (format "0.0.%s" (b/git-count-revs nil)))
 
+(def pom-template
+  [[:licenses
+    [:license
+     [:name "Eclipse Public License"]
+     [:url "https://www.eclipse.org/legal/epl-v10.html"]]]
+   [:developers
+    [:developer
+     [:name "pink-gorilla"]]]
+   [:scm
+    [:url "https://github.com/pink-gorilla/ui-sparklines/"]
+    [:connection "scm:git:git://github.com/pink-gorilla/ui-sparklines.git"]
+    [:developerConnection "scm:git:ssh://git@github.com/pink-gorilla/ui-sparklines.git"]]])
+
+
 (defn jar "build the JAR" [opts]
   (println "Building the JAR")
   (-> opts
       (assoc :lib lib
              :version version
+             :pom-data pom-template
              :transitive true)
       (bb/jar)))
 
